fe85c7229e4621b5632d97116626bb8cc2bb4b5d
MaxdeWilde <maxdwilde13@gmail.com>
2020-04-23 14:28:40 +0200

    deathtrigger script (on trigger enter)

---
+++ Assets/Scripts/DeathTrig.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathTrig : MonoBehaviour
+{
+
+    public bool playerDead;
+
+    void Start()
+    {
+        playerDead = false;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!playerDead && col.CompareTag("Player"))
+            PlayerDeath();
+    }
+
+    void PlayerDeath()
+    {
+        playerDead = true;
+
+        Debug.Log("ded lol");
+
+        Invoke("RestartScene", 1f);
+    }
+
+    void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
