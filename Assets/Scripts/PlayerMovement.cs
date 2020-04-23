8fcab45ca3d42df640610d3f1cb84e6c057350c4
mijkolsmith <mijkolsmith@live.com>
2020-04-23 15:01:39 +0200

    added character controls

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public CharacterController2D controller;
+	float horizontalMove = 0f;
+	public float runSpeed = 40;
+	bool jump = false;
+
+	public void Update()
+	{
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+	}
+
+	public void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+}
