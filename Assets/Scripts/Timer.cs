e8e062d4f4e25128da72398b97e2a72dc46e7634
MaxdeWilde <maxdwilde13@gmail.com>
2020-04-23 15:19:03 +0200

    timertje met score

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Transform timerText;
+    public float counter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += 0.01f;
+        counter = Mathf.Ceil(counter);
+        timerText.GetComponent<Text>().text = "Score: " + counter.ToString();
+    }
+}
